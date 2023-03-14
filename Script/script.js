02398068cd30b20bf36089d0676a5374ec77892a
Aderibigbe Victor Toluwani <aderibigbetoluwani5@gmail.com>
2023-03-14 14:12:15 +0100

    Modified carousel and added some stocks

---
+++ Script/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // Activate carousel
+    $("#carouselExampleIndicators").carousel();
+  
+    // Enable carousel auto sliding
+    $(".carousel").carousel({
+      interval: 1000 // Change interval time here
+    });
+});
