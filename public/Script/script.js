bf38d264869c8f22030d06361de3be100c041594
Victor Aderibigbe <aderibigbetoluwani5@gmail.com>
2023-06-10 14:13:10 +0100

    Testing

--- public/Script/script.js
+++ public/Script/script.js
@@ -156,9 +156,11 @@ const removeFromCart = () => {
       productPrice: userGoods,
       Unit: goodUnit
     }
-    allProducts.splice(0, 1, goodsInfo)
-    localStorage.setItem('localproduct', JSON.stringify(allProducts))
-    console.log('Successfully removed one item from cart');
+    if (quantity.innerText > 0) {
+      allProducts.splice(0, 1, goodsInfo)
+      localStorage.setItem('localproduct', JSON.stringify(allProducts))
+      console.log('Successfully removed one item from cart');
+    }
   })
 }
 
