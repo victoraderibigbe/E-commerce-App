9fba304a54318e41671861b1c2a0ddccb2ce709b
Victor Aderibigbe <aderibigbetoluwani5@gmail.com>
2023-06-10 14:14:23 +0100

    Testing

--- public/Script/script.js
+++ public/Script/script.js
@@ -166,7 +166,7 @@ const removeFromCart = () => {
 
 // Triggers removeFromCart function is quantity is greater than zero
 removeItem.addEventListener('click', ()=> {
-  if(quantity.innerText > 1) {
+  if(quantity.innerText > 0) {
     removeFromCart()
   } 
   else {
